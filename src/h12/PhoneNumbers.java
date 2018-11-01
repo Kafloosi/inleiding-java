26bf694203f2c118bf6b0cd4a9cc3ba346396fcc
Kafloosi <42800267+Kafloosi@users.noreply.github.com>
2018-11-01 20:27:49 +0100

    yeet

---
+++ src/h12/PhoneNumbers.java
@@ -0,0 +1,20 @@
+//Ik snap de rest al dus bij dezen de praktijkopdracht alleen, is dit een probleem moeten jullie het even melden.
+
+package h12;
+
+import java.awt.*;
+
+public class PhoneNumbers
+{
+    
+
+    public void init()
+    {
+
+    }
+
+    public void paint(Graphics g)
+    {
+
+    }
+}
